016fcaf992d0f65740079950d6d3432b5edfe652
Nhatdien <nhatkhonvl@gmail.com>
2025-03-20 09:40:59 +0700

    add GUI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# main.py
 import numpy as np
+import tkinter as tk
+from tkinter import messagebox
 from galois_multiplication import *
 from key_expansion import keyExpansion128
 from aes_encrypt_decrypt import *
@@ -7,103 +8,87 @@ from utils import stringToMat, matToString, xorMatrix
 from sbox_and_rcon import *
 
 
-def encrypt(state=None, key=None):
-    ret = ""
-    while (state == None):
-        print("Please insert your plaintext?")
-        state = input()
-
-    while (key == None or not (len(key) == 16 or len(key) == 32 or len(key) == 24)):
-        print("Please insert your cipher key? Your key must be of length 16, 24 or 32")
-        key = input()
+def encrypt(state, key):
+    if not (len(key) == 16 or len(key) == 32 or len(key) == 24):
+        messagebox.showerror(
+            "Error", "Cipher key must be of length 16, 24, or 32")
+        return ""
 
     lenkey = len(key)
-
-    func = {
-        16: AES128,
-        24: AES192,
-        32: AES256
-    }
-
+    func = {16: AES128, 24: AES192, 32: AES256}
     res = [state[y - 16:y] for y in range(16, len(state) + 16, 16)]
-
     lim = 16 - len(res[-1])
-
-    for i in range(0, lim):
-        res[-1] += chr(0x00)
+    res[-1] += chr(0x00) * lim
 
     key = stringToMat(key)
-    if (lenkey != 16):
-        cypherkey = np.transpose(key)
-        cypherkey = cypherkey.tolist()
-    else:
-        cypherkey = key
+    cypherkey = np.transpose(key).tolist() if lenkey != 16 else key
 
-    print(f"Using AES{lenkey*8} to encrypt")
-    for i in res:
-        sub = stringToMat(i)
-        sub = func[lenkey](sub, cypherkey)
-        sub = matToString(sub)
-        ret += sub
-
-    return ret
+    encrypted_text = "".join(matToString(
+        func[lenkey](stringToMat(i), cypherkey)) for i in res)
+    return encrypted_text
 
 
-def decrypt(state=None, key=None):
-
-    ret = ""
-
-    while (state == None):
-        print("Please insert your plaintext?")
-        state = input()
-
-    while (key == None or not (len(key) == 16 or len(key) == 32 or len(key) ==
-                               24)):
-        print("Please insert your cipher key? Your key must be of length 16, 24 or 32")
-        key = input()
+def decrypt(state, key):
+    if not (len(key) == 16 or len(key) == 32 or len(key) == 24):
+        messagebox.showerror(
+            "Error", "Cipher key must be of length 16, 24, or 32")
+        return ""
 
     lenkey = len(key)
-
-    func = {
-        16: AES128_decrypt,
-        24: AES192_decrypt,
-        32: AES256_decrypt
-    }
-
+    func = {16: AES128_decrypt, 24: AES192_decrypt, 32: AES256_decrypt}
     res = [state[y - 16:y] for y in range(16, len(state) + 16, 16)]
-
     lim = 16 - len(res[-1])
-
-    for i in range(0, lim):
-        res[-1] += chr(0x00)
+    res[-1] += chr(0x00) * lim
 
     key = stringToMat(key)
-    if (lenkey != 16):
-        cypherkey = np.transpose(key)
-        cypherkey = cypherkey.tolist()
-    else:
-        cypherkey = key
+    cypherkey = np.transpose(key).tolist() if lenkey != 16 else key
 
-    for i in res:
-        sub = stringToMat(i)
-        sub = func[lenkey](sub, cypherkey)
-        sub = matToString(sub)
-        ret += sub
-
-    return ret
+    decrypted_text = "".join(matToString(
+        func[lenkey](stringToMat(i), cypherkey)) for i in res)
+    return decrypted_text
 
 
-def main():
-    plain_text = input("Input you plain text: ")
-    key = input(
-        "Please insert your cipher key? Your key must be of length 16, 24 or 32: ")
-    encrypted_state = encrypt(plain_text, key)
-
-    decrypt_state = decrypt(encrypted_state, key=key)
-
-    print(
-        f"Your plain text: {plain_text} ecrypted to {encrypted_state} and will be decrypted back to {decrypt_state} with key: {key}")
+def on_encrypt():
+    plain_text = entry_plain.get()
+    key = entry_key.get()
+    encrypted_text = encrypt(plain_text, key)
+    entry_encrypted.delete(0, tk.END)
+    entry_encrypted.insert(0, encrypted_text)
 
 
-if __name__ == "__main__":
-    main()
+def on_decrypt():
+    encrypted_text = entry_encrypted.get()
+    key = entry_key.get()
+    decrypted_text = decrypt(encrypted_text, key)
+    entry_decrypted.delete(0, tk.END)
+    entry_decrypted.insert(0, decrypted_text)
+
+
+# UI Setup
+root = tk.Tk()
+root.title("AES Encryption & Decryption")
+root.geometry("500x300")
+
+tk.Label(root, text="Plain Text:").grid(row=0, column=0, padx=10, pady=5)
+entry_plain = tk.Entry(root, width=50)
+entry_plain.grid(row=0, column=1, padx=10, pady=5)
+
+tk.Label(root, text="Cipher Key:").grid(row=1, column=0, padx=10, pady=5)
+entry_key = tk.Entry(root, width=50, show="*")
+entry_key.grid(row=1, column=1, padx=10, pady=5)
+
+tk.Button(root, text="Encrypt", command=on_encrypt).grid(
+    row=2, column=0, columnspan=2, pady=10)
+
+tk.Label(root, text="Encrypted Text:").grid(row=3, column=0, padx=10, pady=5)
+entry_encrypted = tk.Entry(root, width=50)
+entry_encrypted.grid(row=3, column=1, padx=10, pady=5)
+
+tk.Button(root, text="Decrypt", command=on_decrypt).grid(
+    row=4, column=0, columnspan=2, pady=10)
+
+tk.Label(root, text="Decrypted Text:").grid(row=5, column=0, padx=10, pady=5)
+entry_decrypted = tk.Entry(root, width=50)
+entry_decrypted.grid(row=5, column=1, padx=10, pady=5)
+
+root.mainloop()
